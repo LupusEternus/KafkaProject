154d929c0c8674b52d6609f0e793787fa6aa1dd3
LupusEternus <wilk.tomek@gmail.com>
2024-06-14 10:58:52 +0200

    added error handling for deserializer

--- Consumer/src/main/java/com/wilk/emailnotificationmicroservice/config/KafkaConsumerConfig.java
+++ Consumer/src/main/java/com/wilk/emailnotificationmicroservice/config/KafkaConsumerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -27,7 +28,8 @@ public class KafkaConsumerConfig {
         Map<String,Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,JsonDeserializer.class);
         config.put(ConsumerConfig.GROUP_ID_CONFIG,groupID);
         config.put(JsonDeserializer.TRUSTED_PACKAGES,"com.wilk.core");
 
